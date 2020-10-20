6986e08691a7e4c586f5eac52e6345b404269ec5
Leonardo Sposina <leonardo.sposina@icloud.com>
2020-10-20 20:40:24 -0300

    feat!: Order entity, controller, service, repository

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,63 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) throw new AppError('This user does not exists', 400);
+
+    const existingProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (products.length !== existingProducts.length)
+      throw new AppError('Invalid product(s) found in this order', 400);
+
+    const productsWithInvalidQuantity = existingProducts.filter(
+      productInStock =>
+        productInStock.quantity <
+        products.filter(
+          productInOrder => productInOrder.id === productInStock.id,
+        )[0].quantity,
+    );
+
+    if (productsWithInvalidQuantity.length)
+      throw new AppError('Invalid product quantity found in this order', 400);
+
+    const productsList = products.map(productInOrder => ({
+      product_id: productInOrder.id,
+      quantity: productInOrder.quantity,
+      price: existingProducts.filter(
+        productInStock => productInStock.id === productInOrder.id,
+      )[0].price,
+    }));
+
+    const order = this.ordersRepository.create({
+      customer: customerExists,
+      products: productsList,
+    });
+
+    const updatedProductsQuantity = products.map(productInOrder => ({
+      id: productInOrder.id,
+      quantity:
+        existingProducts.filter(
+          productInStock => productInStock.id === productInOrder.id,
+        )[0].quantity - productInOrder.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(updatedProductsQuantity);
+
+    return order;
   }
 }
 
